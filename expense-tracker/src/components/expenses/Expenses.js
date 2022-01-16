6ccc9a6200fb6963ed3d143004aa5df0cee21ee2
jasonc-dev <jasoncohen07@gmail.com>
2022-01-16 20:00:49 +0000

    react course commit

---
+++ expense-tracker/src/components/expenses/Expenses.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import ExpenseFilter from "./ExpenseFilter";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import "./ExpenseFilter.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  function filterChangeHandler(selectedYear) {
+    setFilteredYear(selectedYear);
+  }
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
---
+++ replace-redux/src/context/products-context.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+export const ProductsContext = React.createContext({
+  products: [],
+  toggleFav: (id) => {},
+});
+
+export default (props) => {
+  const [productsList, setProductsList] = useState();
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const updatedProducts = [...currentProdList];
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+      return updatedProducts;
+    });
+  };
+
+  return (
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
